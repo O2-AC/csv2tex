0b180db93b66c7f5657b5166af01abdc992fd977
Ole Osterthun <osterthun.ole@gmail.com>
2019-11-06 11:27:27 +0100

    Basic functionality to display tabular code block implemented

---
+++ csv2tex.py
@@ -0,0 +1,45 @@
+import csv
+import sys
+
+
+# This script needs to be called with one argument,
+# e.g. the csv filename. Abort if requirements are 
+# not met.
+
+if len(sys.argv) != 2:
+    print("Usage: csv2tex <file.csv>")
+    sys.exit()
+# read in csv file line by line
+# this will generate the the data array in the form:
+# [[row1],[row2],...] with row1=[x,y,z,...]
+
+data = []
+with open(sys.argv[1]) as csvDataFile:
+    csvReader = csv.reader(csvDataFile)
+    for row in csvReader:
+        data.append(row)
+# print as tex code
+tex = ''
+
+# count columns
+nCol = len(data[0])
+tex = tex + '\\begin{tabular}{l' + (nCol - 1) * 'c' + '}\n\\hline\n'
+# This reads all lines and parses them to tex output 
+for n in range(len(data)):
+    newLine = ''
+    for i in data[n]:
+        newLine = newLine + i + '&' 
+    newLine = newLine[:-1]
+    newLine = newLine + '\\\\'
+    newLine = newLine + '\n'
+    if n == 0:
+        newLine = newLine + '\\hline\n'
+    tex = tex + newLine
+newLine = newLine[:-3]
+newLine = newLine + '\n'
+newLine = newLine + '\\hline'
+tex = tex + newLine
+tex = tex + '\n\\end{tabular}'
+
+print(tex)  
+
